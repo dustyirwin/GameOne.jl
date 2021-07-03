cd7cb7d3e676530e940e12da88b139f830d39b07
dusty <dusty.irwin@gmail.com>
2021-07-02 17:05:02 -0700

    deactivate Redraw...

--- src/GameOne.jl
+++ src/GameOne.jl
@@ -5,7 +5,7 @@ using Random
 
 export Image, Game, game, draw, scheduler, schedule_once, schedule_interval, schedule_unique, unschedule,
         collide, angle, distance, play_music, play_sound, line, clear, rungame, game_include,
-        getEventType, getTextInputEventChar, getTextEditEventString, start_terminal
+        getEventType, getTextInputEventChar, getTextEditEventString, start_terminal, Redraw
 export Keys, Keymods, MouseButtons
 export Line, Rect, Circle
 
@@ -322,7 +322,7 @@ function start_terminal(g::Game, comp="")
             end
         end
         
-        SDL2.Redraw()
+        #Redraw()
     end
 
     SDL2.StopTextInput()
