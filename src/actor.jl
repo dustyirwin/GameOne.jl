223ddc4e88e161842a1f93b5d750476e7c77205c
dusty <dusty.irwin@gmail.com>
2021-11-26 18:35:08 -0800

    SDL2 0.2 compat

--- src/actor.jl
+++ src/actor.jl
@@ -4,7 +4,7 @@ using GameOne
 using ShiftedArrays
 using SimpleDirectMediaLayer
 
-const SDL2 = SimpleDirectMediaLayer.LibSDL2
+const SDL2 = SimpleDirectMediaLayer
 
 mutable struct Actor
     label::String
