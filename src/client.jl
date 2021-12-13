8ff75f9e518a470aa4c837514a3265027ce8cf2e
dusty <dusty.irwin@gmail.com>
2021-12-12 17:59:39 -0800

    init simple websockets client and server

--- src/client.jl
+++ src/client.jl
@@ -1,4 +1,5 @@
 
+module Client
 
 using HTTP
 using JSON
@@ -6,6 +7,7 @@ using Random
 
 HTTP.WebSockets.open("ws://127.0.0.1:8081") do ws
     running = true
+    
     while running
         send_message = Dict(:x => 0.34, :y => "hello")
         data = JSON.json(send_message)
@@ -15,21 +17,17 @@ HTTP.WebSockets.open("ws://127.0.0.1:8081") do ws
         println(message)
         running = message["y"] == "bye" ? false : true
     end
-    println("Exiting...")
-    println()
-end
 
-
-HTTP.WebSockets.open("ws://127.0.0.1:8081") do ws
-    running = true
-    while running
-        send_message = Dict(:id => 1, :y => randstring(5))
-        data = JSON.json(send_message)
-        write(ws, data)
-        recieved_message = readavailable(ws)
-        message = JSON.parse(String(recieved_message))
-        println(message)
-        running = message["y"] == "bye" ? false : true
-    end
     println("Exiting...\n")
 end
+
+HTTP.WebSockets.open("ws://127.0.0.1:8081") do ws
+    send_message = Dict(:id => 1, :y => img)
+    data = JSON.json(send_message)
+    write(ws, data)
+    recieved_message = readavailable(ws)
+    message = JSON.parse(String(recieved_message))
+    println(message)
+end
+
+end # module
