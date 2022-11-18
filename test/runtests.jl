3d6c4522f1bc5d63786a20c097d37ee0a05d57a8
dusty <dusty.irwin@gmail.com>
2022-11-18 15:23:08 -0800

    Converted project to 'dev' for Revise support

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,6 @@
 using GameOne
 using Test
 
-@testset "basic" begin
-    global g
-    @test_nowarn begin 
-        g = GameOne.initgame(joinpath("..","example","BasicGame","basic.jl"))
-        GameOne.quitSDL(g)
-    end
-    
-end
-
-@testset "basic2" begin
-    @test_nowarn begin 
-        g = GameOne.initgame(joinpath("..","example","BasicGame","basic2.jl"))
-        GameOne.quitSDL(g)
-    end
+@testset "GameOne.jl" begin
+    # Write your tests here.
 end
