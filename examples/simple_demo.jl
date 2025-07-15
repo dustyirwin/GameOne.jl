ce2e50327fec291177266bc233b3321503cde4df
Dustin Irwin <dusty.irwin@gmail.com>
2025-07-15 01:37:23 -0700

    Add new dependencies and enhance sound functionality in demo example

--- examples/simple_demo.jl
+++ examples/simple_demo.jl
@@ -1,8 +1,11 @@
 using CImGui
 using FileIO, Colors
-using ImageCore: load, permutedims, Array
+using ImageCore: permutedims, Array
 using ModernGL, GLFW
+using PortAudio, LibSndFile
+using SampledSignals
 
+PA = PortAudio.LibPortAudio
 CImGui.set_backend(:GlfwOpenGL3)
 
 ctx = CImGui.CreateContext()
@@ -11,6 +14,16 @@ io = CImGui.GetIO()
 io.ConfigFlags = unsafe_load(io.ConfigFlags) | CImGui.ImGuiConfigFlags_DockingEnable
 io.ConfigFlags = unsafe_load(io.ConfigFlags) | CImGui.ImGuiConfigFlags_ViewportsEnable
 
+# sound file setup
+sound_id = Ref{Any}(nothing)
+function play_sound(file_path::String)
+    buf = load(file_path)
+    stream = PortAudioStream(0, nchannels(buf); samplerate=samplerate(buf))
+    write(stream, buf)
+    close(stream)
+end
+
+# load an image
 image_id = Ref{Any}(nothing)
 image_path = joinpath(@__DIR__, "images", "alien.png")
 
@@ -28,6 +41,12 @@ CImGui.render(ctx) do
     CImGui.update_image_texture(image_id[], img_data_gl_flat, img_w, img_h)
     if CImGui.Begin("Image Window")
         CImGui.Image(image_id[], CImGui.ImVec2(img_w, img_h))
+        CImGui.End()
+    end
+    if CImGui.Begin("Sound Control")
+        if CImGui.Button("Play Sound")
+            play_sound(joinpath(@__DIR__, "sounds", "eep.wav"))
+        end
         CImGui.End()
     end
 end
--- src/GameOne.jl
+++ src/GameOne.jl
@@ -42,7 +42,7 @@ using Reexport: @reexport
 # Modern exports
 export game, draw, render, flush!, scheduler, schedule_once, schedule_interval, schedule_unique, unschedule,
     collide, angle, distance, play_music, play_sound, line, clear, rungame, game_include,
-    window_paused, start_text_input, update_text_actor!,
+    window_paused, start_text_input, update_text_actor!, create_gl_context,
     load_texture, create_shader, compile_shader, use_shader, bind_texture, my_imgui_preinit,
     begin_batch, end_batch, draw_quad, draw_sprite, draw_text, create_screen, load_animated_textures, move!
 export Game, Screen, GLContext, Renderer, Shader, Texture, BatchRenderer
