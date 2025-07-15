bf7a06c63e469495d9f786dda87292a69969e1f9
Dustin Irwin <dusty.irwin@gmail.com>
2025-07-15 00:37:38 -0700

    Undistorted alien img!

--- examples/simple_demo.jl
+++ examples/simple_demo.jl
@@ -1,79 +1,28 @@
 using CImGui
-using CImGui.lib
-using CImGui.CSyntax
-import GLFW
-import ModernGL as GL
-using Colors
-using FileIO
-using ImageIO
-using ImageCore: channelview
+using FileIO, Colors
+using ImageCore: load, permutedims, Array
+using ModernGL, GLFW
 
-
-# Set up the backend
 CImGui.set_backend(:GlfwOpenGL3)
-
-# Create ImGui context and enable docking/viewports
 ctx = CImGui.CreateContext()
-io = CImGui.GetIO()
-io.ConfigFlags = unsafe_load(io.ConfigFlags) | CImGui.ImGuiConfigFlags_DockingEnable
-io.ConfigFlags = unsafe_load(io.ConfigFlags) | CImGui.ImGuiConfigFlags_ViewportsEnable
-CImGui.StyleColorsDark()
-style = Ptr{CImGui.ImGuiStyle}(CImGui.GetStyle())
-if unsafe_load(io.ConfigFlags) & CImGui.ImGuiConfigFlags_ViewportsEnable == CImGui.ImGuiConfigFlags_ViewportsEnable
-    style.WindowRounding = 5.0f0
-    col = CImGui.c_get(style.Colors, CImGui.ImGuiCol_WindowBg)
-    CImGui.c_set!(style.Colors, CImGui.ImGuiCol_WindowBg, CImGui.ImVec4(col.x, col.y, col.z, 1.0f0))
-end
 
-# Load an image as a texture (replace with your image path)
-img_width, img_height = 256, 256
 image_id = Ref{Any}(nothing)
-image_path = joinpath(@__DIR__,"images","alien.png")  # <-- set this to your image
-
-
-# Use FileIO and ImageIO to load images as arrays
+image_path = joinpath(@__DIR__, "images", "alien.png")
 
 img = load(image_path)
-@info "Loaded image" typeof=typeof(img) eltype=eltype(img) size=size(img)
+img_rgba = Array(img)  # Already RGBA, just ensure Array
 
-if eltype(img) <: Colors.RGBA
-    img_rgba = img
-elseif ndims(img) == 2
-    imgf = float.(img)
-    img_rgba = RGBA{N0f8}.(imgf, imgf, imgf, 1)
-elseif ndims(img) == 3 && size(img, 3) == 3
-    imgf1 = float.(img[:,:,1])
-    imgf2 = float.(img[:,:,2])
-    imgf3 = float.(img[:,:,3])
-    img_rgba = RGBA{N0f8}.(imgf1, imgf2, imgf3, 1)
-elseif ndims(img) == 3 && size(img, 3) == 4
-    imgf1 = float.(img[:,:,1])
-    imgf2 = float.(img[:,:,2])
-    imgf3 = float.(img[:,:,3])
-    imgf4 = float.(img[:,:,4])
-    img_rgba = RGBA{N0f8}.(imgf1, imgf2, imgf3, imgf4)
-else
-    error("Unsupported image format")
-end
+img_h, img_w = size(img_rgba)  # Julia: (height, width)
+img_gl = permutedims(img_rgba, (2, 1))  # (width, height)
+img_data_gl_flat = vec(reinterpret(UInt8, img_gl))
 
-
-h, w = size(img_rgba)
-@info "img_rgba shape" shape=size(img_rgba) eltype=eltype(img_rgba)
-
-# Fix: flatten RGBA array in row-major order for OpenGL
-img_data_gl = permutedims(img_rgba, (2,1))  # (w, h)
-@info "img_data_gl shape" shape=size(img_data_gl)
-@info "Texture dimensions" w=w h=h
-img_data_gl_flat = vec(reinterpret(UInt32, img_data_gl))
-
- # Create the OpenGL texture
 CImGui.render(ctx) do
     if image_id[] === nothing
-        image_id[] = CImGui.create_image_texture(w, h)
+        image_id[] = CImGui.create_image_texture(img_w, img_h)
     end
-    CImGui.update_image_texture(image_id[], img_data_gl_flat, w, h)
+    CImGui.update_image_texture(image_id[], img_data_gl_flat, img_w, img_h)
     if CImGui.Begin("Image Window")
-        CImGui.Image(image_id[], CImGui.ImVec2(w, h))
+        CImGui.Image(image_id[], CImGui.ImVec2(img_w, img_h))
         CImGui.End()
     end
 end
